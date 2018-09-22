c206c5a00f746bdcda17230942a2e7a785aedf96
Baptiste Langlade <baptouuuu@gmail.com>
2018-09-22 15:01:39 +0200

    expose logger command bus via bootstrap function

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace Innmind\CommandBus;
 
 use Innmind\Immutable\MapInterface;
+use Psr\Log\LoggerInterface;
 
 function bootstrap(): array
 {
@@ -16,6 +17,11 @@ function bootstrap(): array
         'enqueue' => new EnqueueCommandBus($queue),
         'dequeue' => static function(CommandBusInterface $bus) use ($queue): CommandBusInterface {
             return new DequeueCommandBus($bus, $queue);
+        },
+        'logger' => static function(LoggerInterface $logger): callable {
+            return static function(CommandBusInterface $bus) use ($logger): CommandBusInterface {
+                return new LoggerCommandBus($bus, $logger);
+            };
         },
     ];
 }
